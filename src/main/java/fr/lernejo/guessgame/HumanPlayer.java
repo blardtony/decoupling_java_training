5044a3169742ec6c69c9cabddd0eff6e12be293f
Tony Blard <tony.blard@epita.fr>
2022-02-11 12:34:48 +0100

    Partie 3

---
+++ src/main/java/fr/lernejo/guessgame/HumanPlayer.java
@@ -0,0 +1,22 @@
+package fr.lernejo.guessgame;
+
+import fr.lernejo.logger.Logger;
+import fr.lernejo.logger.LoggerFactory;
+
+import java.util.Scanner;
+
+public class HumanPlayer implements Player{
+    private final Logger logger = LoggerFactory.getLogger("player");
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public long askNextGuess() {
+        return 0;
+    }
+
+    @Override
+    public void respond(boolean lowerOrGreater) {
+
+    }
+
+}
---
+++ src/main/java/fr/lernejo/guessgame/Simulation.java
@@ -0,0 +1,41 @@
+package fr.lernejo.guessgame;
+
+import fr.lernejo.logger.Logger;
+import fr.lernejo.logger.LoggerFactory;
+
+public class Simulation {
+
+    private final Logger logger = LoggerFactory.getLogger("simulation");
+    private final Player player;  //TODO add variable type
+    private long numberToGuess; //TODO add variable type
+
+    public Simulation(Player player) {
+        this.player = player;
+    }
+
+    public void initialize(long numberToGuess) {
+        this.numberToGuess = numberToGuess;
+    }
+
+    /**
+     * @return true if the player have guessed the right number
+     */
+    private boolean nextRound() {
+        //TODO implement me
+        long numPlayerAsk = this.player.askNextGuess();
+        if (numPlayerAsk == this.numberToGuess) {
+            this.logger.log("Player find number");
+            return true;
+        }
+        this.player.respond(numPlayerAsk > this.numberToGuess);
+        this.logger.log("Player didn't find the number");
+        return false;
+    }
+
+    public void loopUntilPlayerSucceed() {
+        boolean end = false;
+        while (end) {
+            end = this.nextRound();
+        }
+    }
+}
